ed7fac929c7b056fba3a94e98c76994693a912b7
Dimitris Dallas <dimitrisdallas@Dimitriss-MacBook-Pro-2.local>
2025-11-10 14:12:38 +0100

    intro image missing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
+import intro from '../images/intro.jpg'
 import work from '../images/work.jpg'
 import about from '../images/about.jpg'
 
@@ -30,7 +30,7 @@ class Main extends React.Component {
         >
           <h2 className="major">Intro</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={intro} alt="" />
           </span>
           <p>
             Noimo ApS is a technology company specialised in data analytics and data engineering solutions.
